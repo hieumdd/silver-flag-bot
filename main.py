2ef6c0ec1f9888643edbf5f3e244058714c6b4e8
HM <hieumdd@gmail.com>
2024-01-23 14:20:00 +0700

    feat: Add telegram app

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-import time
+import json
+import os
 
-import schedule
+from telegram.ext import Application, ContextTypes
+from ssi.client_model import GetIntradayOptions
+from ssi.client_service import SSIClient
+
+CHAT_ID = -4154075164
+
+ssi_client = SSIClient()
 
 
-def main():
-    print("123")
+async def main(context: ContextTypes.DEFAULT_TYPE):
+    data = ssi_client.get_intraday(GetIntradayOptions("VN30F1M"))["data"][0]
+    await context.bot.send_message(
+        chat_id=CHAT_ID,
+        text=f"""<pre>{json.dumps(data, indent=2)}</pre>""",
+        parse_mode="html",
+    )
 
 
-schedule.every().minute.do(main)
+application = Application.builder().token(os.getenv("TELEGRAM_TOKEN", "")).build()
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+application.job_queue.run_repeating(main, interval=5, first=0)
+
+application.run_polling()
--- telegram.py
+++
@@ -1,5 +0,0 @@
-import os
-
-from telegram import Bot
-
-telegram_bot = Bot(os.getenv("TELEGRAM_TOKEN"))
