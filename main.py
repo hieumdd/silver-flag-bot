e5cfff3bfda53be30c8377aa54e25be0a095a5be
HM <hieumdd@gmail.com>
2024-01-29 21:43:25 +0700

    feat: Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-import json
 import os
+from datetime import time
+from zoneinfo import ZoneInfo
 
 from telegram.ext import Application, ContextTypes
-from ssi.client_model import GetIntradayOptions
-from ssi.client_service import SSIClient
+
+from trading.strategy.multi_ma import MultiMA
 
 CHAT_ID = -4154075164
 
-ssi_client = SSIClient()
+strategy = MultiMA()
 
 
 async def main(context: ContextTypes.DEFAULT_TYPE):
-    data = ssi_client.get_intraday(GetIntradayOptions("VN30F1M"))["data"][0]
-    await context.bot.send_message(
-        chat_id=CHAT_ID,
-        text=f"""<pre>{json.dumps(data, indent=2)}</pre>""",
-        parse_mode="html",
-    )
+    signals = strategy.get_signals()
+
+    if signals:
+        for signal in signals:
+            await context.bot.send_message(
+                chat_id=CHAT_ID,
+                text=str(signal),
+                parse_mode="html",
+            )
 
 
 application = Application.builder().token(os.getenv("TELEGRAM_TOKEN", "")).build()
-
-application.job_queue.run_repeating(main, interval=5, first=0)
-
+application.job_queue.run_repeating(
+    main,
+    interval=60,
+    first=time(9, tzinfo=ZoneInfo("Asia/Ho_Chi_Minh")),
+    last=time(11, 30, tzinfo=ZoneInfo("Asia/Ho_Chi_Minh")),
+)
+application.job_queue.run_repeating(
+    main,
+    interval=60,
+    first=time(13, tzinfo=ZoneInfo("Asia/Ho_Chi_Minh")),
+    last=time(14, 30, tzinfo=ZoneInfo("Asia/Ho_Chi_Minh")),
+)
 application.run_polling()
