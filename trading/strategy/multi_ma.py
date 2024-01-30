8a043a730ed4f3417f7f935016ae88e31edff488
HM <hieumdd@gmail.com>
2024-01-30 20:54:12 +0700

    misc: Update MultiMA

--- trading/strategy/multi_ma.py
+++ trading/strategy/multi_ma.py
@@ -3,7 +3,6 @@ from functools import reduce
 import operator
 
 import pandas as pd
-from pandas.core.series import Series
 import pandas_ta as ta
 
 from ssi.options import GetIntradayOptions
@@ -28,8 +27,9 @@ class MultiMA(Strategy):
     def populate_indicators(self, df):
         _df = df.copy()
 
-        _df["RSI_CROSS_ABOVE"] = ta.cross_value(ta.rsi(_df["close"]), 50)
-        _df["RSI_CROSS_BELOW"] = ta.cross_value(ta.rsi(_df["close"]), 50, above=False)
+        _df["RSI"] = ta.rsi(_df["close"])
+        _df["RSI_CROSS_ABOVE"] = ta.cross_value(_df["RSI"], 50)
+        _df["RSI_CROSS_BELOW"] = ta.cross_value(_df["RSI"], 50, above=False)
         for length in self._range:
             _df[f"EMA_{length}"] = ta.ema(_df["close"], length=length)
         return _df
@@ -37,18 +37,16 @@ class MultiMA(Strategy):
     def populate_signals(self, df):
         _df = df.copy()
 
-        for length in self._range:
-            _df[f"EMA_{length}_DIFF"] = _df[f"EMA_{length}"] - _df[
-                f"EMA_{length}"
-            ].shift(1)
-
         for signal_type in [LongEntry, ShortEntry]:
             _df[[signal_type.flag_col, signal_type.tag_col]] = (None, None)
 
         _df.loc[
             reduce(
                 operator.and_,
-                [_df[f"EMA_{length}_DIFF"] > 0 for length in self._range]
+                [
+                    _df[f"EMA_{length}"] > _df[f"EMA_{length}"].shift(1)
+                    for length in self._range
+                ]
                 + [_df["RSI_CROSS_ABOVE"] == 1],
             ),
             [LongEntry.flag_col, LongEntry.tag_col],
@@ -57,7 +55,10 @@ class MultiMA(Strategy):
         _df.loc[
             reduce(
                 operator.and_,
-                [_df[f"EMA_{length}_DIFF"] < 0 for length in self._range]
+                [
+                    _df[f"EMA_{length}"] < _df[f"EMA_{length}"].shift(1)
+                    for length in self._range
+                ]
                 + [_df["RSI_CROSS_BELOW"] == 1],
             ),
             [ShortEntry.flag_col, ShortEntry.tag_col],
