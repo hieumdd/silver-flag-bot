ff5217e4903291a43115ece149746c7229e1a60b
HM <hieumdd@gmail.com>
2024-01-30 09:53:20 +0700

    fix: Fix loggings

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from trading.strategy.multi_ma import MultiMA
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 timezone = ZoneInfo("Asia/Ho_Chi_Minh")
 chat_id = -4154075164
@@ -16,11 +20,11 @@ strategy = MultiMA()
 
 
 async def main(context: ContextTypes.DEFAULT_TYPE):
-    logger.debug(f"Running strategy {strategy.__class__}")
+    logger.info(f"Running strategy {strategy.__class__}")
 
     if signals := strategy.get_signals():
-        logger.debug(f"Signal: {str(signal)}")
         for signal in signals:
+            logger.info(f"Signal: {str(signal)}")
             await context.bot.send_message(
                 chat_id=chat_id,
                 text=str(signal),
