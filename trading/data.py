478c2a826b91d9d80ea4cd00bb925884961422c6
HM <hieumdd@gmail.com>
2024-03-05 11:24:20 +0700

    refactor: Refactor GetIntradayOptions

--- trading/data.py
+++ trading/data.py
@@ -1,15 +1,26 @@
+from abc import abstractmethod
 from datetime import datetime
 import pandas as pd
-from typing import Protocol
 
 from ssi.client import SSIClient
-from ssi.options import GetIntradayOptions
 from trading.timeframe import Timeframe
 
 
-class DataProvider(Protocol):
+class DataProvider:
+    timeframe: Timeframe
+    ohlc_columns = {
+        "open": "first",
+        "high": "max",
+        "low": "min",
+        "close": "last",
+        "volume": "sum",
+    }
     client = SSIClient()
 
+    def __init__(self, timeframe: Timeframe):
+        self.timeframe = timeframe
+
+    @abstractmethod
     def get(self, *args, **kwargs) -> pd.DataFrame:
         pass
 
@@ -21,31 +32,25 @@ class DataProvider(Protocol):
 
 
 class IntradayDataProvider(DataProvider):
-    def __init__(self, timeframe: Timeframe):
-        self.timeframe = timeframe
-
     def get(self, symbol: str) -> pd.DataFrame:
-        data = self.client.get_intraday(GetIntradayOptions(symbol))
+        start_date, *_, end_date = pd.bdate_range(
+            end=datetime.today().date(),
+            periods=7,
+        )
+        data = self.client.get_intraday(symbol, start_date, end_date)
+
         df = pd.DataFrame(data).drop_duplicates()
         df["timestamp"] = pd.DatetimeIndex(df.apply(self.create_timestamp, axis=1))
-
-        ohlc_columns = {
-            "open": "first",
-            "high": "max",
-            "low": "min",
-            "close": "last",
-            "volume": "sum",
-        }
 
         df = (
             (
                 df.set_index(df["timestamp"], drop=False)
                 .sort_index()
                 .rename(str.lower, axis=1)
-                .astype({col_name: float for col_name in ohlc_columns})
-            )[["symbol", "timestamp", *ohlc_columns.keys()]]
+                .astype({col_name: float for col_name in self.ohlc_columns})
+            )[["symbol", "timestamp", *self.ohlc_columns.keys()]]
             .resample(self.timeframe.interval)
-            .agg(ohlc_columns)
+            .agg(self.ohlc_columns)
             .dropna()
         )
 
