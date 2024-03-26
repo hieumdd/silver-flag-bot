cba3b87fe9ac572cf32730daac002b4e765817fc
HM <hieumdd@gmail.com>
2024-03-26 22:45:36 +0700

    feat: Update ATRTrailingStop

--- trading/strategy/atr_trailing_stop.py
+++ trading/strategy/atr_trailing_stop.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas_ta as ta
+import mplfinance as mpf
 
 from trading.timeframe import TF_5MIN
 from trading.data import IntradayDataProvider
@@ -10,10 +11,15 @@ from trading.strategy.interface import Strategy
 class ATRTrailingStop(Strategy):
     data_provider = IntradayDataProvider(TF_5MIN)
 
-    atr_length = 10
-    n_loss_sensitivity = 2
+    def __init__(self, symbol, atr_length=50, n_loss_sensitivity=2, ma_period=2):
+        super().__init__(symbol)
+        self.atr_length = atr_length
+        self.n_loss_sensitivity = n_loss_sensitivity
+        self.ma_period = ma_period
 
     def populate_indicators(self, df):
+        df["SRC"] = df["open"]
+
         df["ATR"] = ta.atr(
             df["high"],
             df["low"],
@@ -28,39 +34,57 @@ class ATRTrailingStop(Strategy):
         df.loc[df.index[0], "ATR_TRAILING_STOP"] = 0.0
 
         for i in range(1, len(df)):
-            close = df.loc[df.index[i], "close"]
-            prev_close = df.loc[df.index[i - 1], "close"]
+            src = df.loc[df.index[i], "SRC"]
+            prev_src = df.loc[df.index[i - 1], "SRC"]
             prev_atr_trailing_stop = df.loc[df.index[i - 1], "ATR_TRAILING_STOP"]
             n_loss = df.loc[df.index[i], "NLOSS"]
 
             atr_trailing_stop = 0
 
-            if close > prev_atr_trailing_stop and prev_close > prev_atr_trailing_stop:
-                atr_trailing_stop = max(prev_atr_trailing_stop, close - n_loss)
-            elif close < prev_atr_trailing_stop and prev_close < prev_atr_trailing_stop:
-                atr_trailing_stop = min(prev_atr_trailing_stop, close + n_loss)
-            elif close > prev_atr_trailing_stop:
-                atr_trailing_stop = close - n_loss
+            if src > prev_atr_trailing_stop and prev_src > prev_atr_trailing_stop:
+                atr_trailing_stop = max(prev_atr_trailing_stop, src - n_loss)
+            elif src < prev_atr_trailing_stop and prev_src < prev_atr_trailing_stop:
+                atr_trailing_stop = min(prev_atr_trailing_stop, src + n_loss)
+            elif src > prev_atr_trailing_stop:
+                atr_trailing_stop = src - n_loss
             else:
-                atr_trailing_stop = close + n_loss
+                atr_trailing_stop = src + n_loss
 
             df.loc[df.index[i], "ATR_TRAILING_STOP"] = atr_trailing_stop
 
-        df["EMA"] = ta.ema(df["close"], 1, talib=False)
+        df["MA"] = ta.ema(df["SRC"], self.ma_period, talib=False)
 
         return df
 
     def populate_signals(self, df):
         df.loc[
-            (df["close"] > df["ATR_TRAILING_STOP"])
-            & (ta.cross(df["EMA"], df["ATR_TRAILING_STOP"], above=True) == 1),
+            (df["SRC"] > df["ATR_TRAILING_STOP"])
+            & (ta.cross(df["MA"], df["ATR_TRAILING_STOP"], above=True) == 1),
             LongEntry.flag_col,
         ] = True
 
         df.loc[
-            (df["close"] < df["ATR_TRAILING_STOP"])
-            & (ta.cross(df["EMA"], df["ATR_TRAILING_STOP"], above=False) == 1),
+            (df["SRC"] < df["ATR_TRAILING_STOP"])
+            & (ta.cross(df["MA"], df["ATR_TRAILING_STOP"], above=False) == 1),
             ShortEntry.flag_col,
         ] = True
+        df.loc[df[ShortEntry.flag_col] == True, 'ShortValue'] = df["SRC"]
 
         return df
+
+    def populate_subplots(self, df):
+        return [
+            mpf.make_addplot(
+                df["ATR_TRAILING_STOP"],
+                panel=0,
+                width=1,
+                secondary_y=False,
+            ),
+            mpf.make_addplot(
+                df["MA"],
+                panel=0,
+                width=1,
+                linestyle="--",
+                secondary_y=False,
+            ),
+        ]
