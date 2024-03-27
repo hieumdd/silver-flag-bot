6983c218bfd08a3b3da5fb492e20b1f872e84503
HM <hieumdd@gmail.com>
2024-03-27 13:24:54 +0700

    misc: Add loggings to polling

--- bot/polling.py
+++ bot/polling.py
@@ -1,21 +1,25 @@
 from telegram.ext import ContextTypes
 
 from logger import get_logger
+from trading.strategy.interface import Strategy
 
 logger = get_logger(__name__)
 
 
 async def polling(context: ContextTypes.DEFAULT_TYPE):
-    strategy = context.job.data
+    strategy: Strategy = context.job.data
     chat_id = context.job.chat_id
     logger.info(f"Polling strategy {strategy.__class__}")
 
     analysis, signal = strategy.analyze()
 
     if signal:
+        logger.info(f"Signal found for strategy {strategy.__class__}")
         await context.bot.send_photo(
             chat_id=chat_id,
             photo=analysis.plot,
             caption=signal.to_html(analysis.symbol),
             parse_mode="html",
         )
+    else:
+        logger.debug(f"Signal not found for strategy {strategy.__class__}")
