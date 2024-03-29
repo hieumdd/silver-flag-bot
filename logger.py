68368d1b5541e5ee27f68776980708c4ed7f43e8
HM <hieumdd@gmail.com>
2024-03-29 16:48:46 +0700

    misc: Reformat init_logger

--- logger.py
+++ logger.py
@@ -6,12 +6,9 @@ from pythonjsonlogger.jsonlogger import JsonFormatter
 def init_logger():
     logging.basicConfig(handlers=[logging.StreamHandler()])
     for handler in logging.root.handlers:
-        handler.setFormatter(
-            JsonFormatter(
-                "%(name)s - %(levelname)s - %(message)s",
-                rename_fields={"levelname": "severity", "name": "module"},
-            )
-        )
+        format_ = "%(name)s - %(levelname)s - %(message)s"
+        rename_fields = {"levelname": "severity", "name": "module"}
+        handler.setFormatter(JsonFormatter(format_, rename_fields))
 
 
 def get_logger(name: str):
