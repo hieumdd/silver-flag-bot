2a619d58b1defb981835d76c3430498f5f7d609d
HM <hieumdd@gmail.com>
2024-03-29 16:29:22 +0700

    feat: Add analysis to message

--- bot/polling.py
+++ bot/polling.py
@@ -1,5 +1,5 @@
 from telegram.ext import ContextTypes
-from telegram.constants import ChatAction, ParseMode
+from telegram.constants import ParseMode
 
 from logger import get_logger
 from trading.strategy.interface import Strategy
@@ -16,10 +16,7 @@ async def on_polling(context: ContextTypes.DEFAULT_TYPE):
 
     if signal:
         logger.info(f"Signal found for strategy {strategy.__class__}")
-        await context.bot.send_chat_action(
-            chat_id=chat_id,
-            action=ChatAction.UPLOAD_PHOTO,
-        )
+        await context.bot.send_message(chat_id=chat_id, text=analysis.to_html())
         await context.bot.send_photo(
             chat_id=chat_id,
             photo=analysis.plot,
